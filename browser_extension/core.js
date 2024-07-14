71bd3af1737356d62efc7991321c9489cde833cc
Gareth Glendiuz Yauwira <92643002+OMCSB@users.noreply.github.com>
2024-07-14 20:37:56 +0800

    Add `async/fetch/await` function to get response from server and parse JSON
    - Also delete `renderBubble` function and all dependency it has

--- browser_extension/core.js
+++ browser_extension/core.js
@@ -1,27 +1,3 @@
-// Add bubble to the top of the page.
-var bubbleDOM = document.createElement('div');
-bubbleDOM.setAttribute('class', 'selection_bubble');
-document.body.appendChild(bubbleDOM);
-
-// document.addEventListener('mouseover', function(event) {
-//     var hoveredEl = event.target; // The actual element which was hovered.
-//     console.log(hoveredEl.href); // Do what we want here!
-// });
-
-// // Lets listen to mouseup DOM events.
-// document.addEventListener('mouseover', function (e) {
-//   var selection = window.getSelection().toString();
-//   if (selection.length > 0) {
-//     renderBubble(e.clientX, e.clientY, selection);
-//   }
-// }, false);
-
-
-// // Close the bubble when we click on the screen.
-// document.addEventListener('mousedown', function (e) {
-//   bubbleDOM.style.visibility = 'hidden';
-// }, false);
-
 document.addEventListener('click', async function(event) {
   // Check if the clicked element is a link with an href attribute
   if (event.target.tagName === 'A' && event.target.href) {
@@ -30,63 +6,53 @@ document.addEventListener('click', async function(event) {
     
     // Generate Token
     var token = new Date();
+    let jsonToken = token.toJSON(); 
 
     // Log the href value
     console.log('Link clicked: ' + event.target.href);
 
     // Wait for prediction
-    const prom = await predictSelection(event.target.href, token);
+    const predResponse = await predictSelection(event.target.href, token);
 
     // Get response
-    const response = parseOutput();
-    console.log(response)
-
-    // Get User Confirmation
-    if (prom == 1){
-      const userConfirm = confirm("Are you sure you want to continue?\n \nWhatever happens after this is outside of our jurisdiction!")
-      if (userConfirm){
-        window.location.href = event.target.href;
-      } 
-      console.log("Good Choice :)")
+    const getUrl = async function() {
+      try{
+        const promise = await fetch('http://localhost:5000/get_url');
+        const promiseObj = await promise.json();
+        return promiseObj;
+      } catch (error){
+        console.log('Ran into an Error: ${error}')
+      }
     };
+
+    (async () => {
+      // Await the promise returned by getUrl
+      const renderUrl = await getUrl(); 
+      
+      // Check if renderUrl is not null or undefined
+      if (renderUrl) {
+        let findJsonValue = renderUrl.find(item => item.sesh_token === jsonToken);
+        if (findJsonValue) {
+          let result = findJsonValue["result"];
+          let chance = findJsonValue["chance"];
+          let time_taken = findJsonValue["time_taken"];
+
+          if (predResponse == 1){
+            setTimeout(() => {
+              const userConfirm = confirm(`Are you sure you want to continue?\n\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\nThe Link is ${chance}% accurate of being a ${result} link !\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\nWhatever happens after this is outside of our jurisdiction!\n\nTime taken to complete: ${time_taken.toFixed(2)} seconds`);
+              if (userConfirm){
+                window.location.href = event.target.href;
+              } 
+              console.log("Good Choice :)"); 
+            }, 1000);
+          };
+        } else {
+          console.log('No matching token found');
+        }
+      }
+    })();
   }
 }, false);
-
-// Move that bubble to the appropriate location.
-function renderBubble(mouseX, mouseY, token) {
-  jObj = parseOutput()
-  if (token == jObj.sesh_token){
-    bubbleDOM.innerHTML = "This link is ", jObj.chance, "% ", jObj.result;
-    bubbleDOM.style.top = mouseY + 'px';
-    bubbleDOM.style.left = mouseX + 'px';
-    bubbleDOM.style.visibility = 'visible';
-  } else {
-    console.log("Mismatched Token!")
-  }
-  
-}
-
-// Parse Data from HTTP Request
-function parseOutput(){
-  getCalculation().then(data => {
-    data;
-  });
-  const respObj = JSON.parse(data);
-  return respObj
-}
-
-// Get Data through Fetch
-async function getCalculation(){
-  try {
-    let response = await fetch('http://localhost:5000/get_url');
-    let data = await response.json();
-
-    // console.log(data)
-    return data;
-  } catch (error) {
-    console.error('Error while fetching', error);
-  }
-}
 
 // Forward HTTP Request with Method POST
 function predictSelection(selection, token){
