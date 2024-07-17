5f9cd663790b740cc4613944fed57c3b6b57019f
Gareth Glendiuz Yauwira <92643002+OMCSB@users.noreply.github.com>
2024-07-17 15:10:48 +0800

    Added 'real-time detection' feature

--- browser_extension/core.js
+++ browser_extension/core.js
@@ -1,58 +1,136 @@
-document.addEventListener('click', async function(event) {
-  // Check if the clicked element is a link with an href attribute
-  if (event.target.tagName === 'A' && event.target.href) {
-    // Prevent the default action (navigation)
-    event.preventDefault();
+processLinks();
+
+// document.addEventListener("mouseover", function(event){
+//   currEl = event.target;
+//   if(currEl.tagName === 'H3'){
+//       parent
+//       console.log(currEl.parentElement.nodeName)
+//   }
+// });
+
+// document.addEventListener('click', async function(event) {
+//   // Check if the clicked element is a link with an href attribute
+//   if (event.target.tagName === 'A' && event.target.href) {
+//     // Prevent the default action (navigation)
+//     event.preventDefault();
     
-    // Generate Token
+//     // Generate Token
+//     var token = new Date();
+//     let jsonToken = token.toJSON(); 
+
+//     // Log the href value
+//     console.log('Link clicked: ' + event.target.href);
+
+//     // Wait for prediction
+//     const predResponse = await predictSelection(event.target.href, token);
+//     const getUrl = await getPrediction();
+//     console.log(getUrl);
+
+//     (async () => {
+//       // Check if renderUrl is not null or undefined
+//       if (getUrl) {
+//         let findJsonValue = getUrl.find(item => item.sesh_token === jsonToken);
+//         if (findJsonValue) {
+//           let result = findJsonValue["result"];
+//           let chance = findJsonValue["chance"];
+//           let time_taken = findJsonValue["time_taken"];
+
+//           if (predResponse == 1){
+//             setTimeout(() => {
+//               const userConfirm = confirm(`Are you sure you want to continue?\n\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\nThe Link is ${chance}% accurate of being a ${result} link !\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\nWhatever happens after this is outside of our jurisdiction!\n\nTime taken to complete: ${time_taken.toFixed(2)} seconds`);
+//               if (userConfirm){
+//                 window.location.href = event.target.href;
+//               } 
+//               console.log("Good Choice :)"); 
+//             }, 1000);
+//           };
+//         } else {
+//           console.log('No matching token found');
+//         }
+//       }
+//     })();
+//   }
+// }, false);
+
+
+// Get a node list of every child elements that matches the selector (.yuRUbf > div > span > a)
+// Matches every anchor element under the '.yuRUbf' class's div's span element  
+async function processLinks() {
+  var links = document.querySelectorAll('.yuRUbf > div > span > a');
+  var urls = {};
+  let linkCnt = 0;
+  console.log("Please wait while we search for malicious links");
+
+  for (let link of links) {
     var token = new Date();
-    let jsonToken = token.toJSON(); 
+    var jsonTok = token.toJSON();
 
-    // Log the href value
-    console.log('Link clicked: ' + event.target.href);
+    // Extracts the HREF from each found in <a> element
+    var extLink = link.getAttribute('href');
 
-    // Wait for prediction
-    const predResponse = await predictSelection(event.target.href, token);
+    // Waits for promise response (1 or print error in console) from predictSelection function
+    const postUrlResult = await predictSelection(extLink, token);
 
-    // Get response
-    const getUrl = async function() {
-      try{
-        const promise = await fetch('http://localhost:5000/get_url');
-        const promiseObj = await promise.json();
-        return promiseObj;
-      } catch (error){
-        console.log('Ran into an Error: ${error}')
-      }
-    };
+    if (postUrlResult == 1) {
+      // Fetches URL in JSON format
+      const getUrlResult = await getPrediction();
 
-    (async () => {
-      // Await the promise returned by getUrl
-      const renderUrl = await getUrl(); 
-      
-      // Check if renderUrl is not null or undefined
-      if (renderUrl) {
-        let findJsonValue = renderUrl.find(item => item.sesh_token === jsonToken);
-        if (findJsonValue) {
-          let result = findJsonValue["result"];
-          let chance = findJsonValue["chance"];
-          let time_taken = findJsonValue["time_taken"];
+      if (getUrlResult) {
+        // Returns 3 values from the parseJsonVal function
+        let [gRes, gChan, gTT] = parseJsonVal(getUrlResult, jsonTok);
 
-          if (predResponse == 1){
-            setTimeout(() => {
-              const userConfirm = confirm(`Are you sure you want to continue?\n\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\nThe Link is ${chance}% accurate of being a ${result} link !\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\nWhatever happens after this is outside of our jurisdiction!\n\nTime taken to complete: ${time_taken.toFixed(2)} seconds`);
-              if (userConfirm){
-                window.location.href = event.target.href;
-              } 
-              console.log("Good Choice :)"); 
-            }, 1000);
-          };
-        } else {
-          console.log('No matching token found');
+        // Append the values to 'urls' dictionary
+        urls[extLink] = { "Result": gRes, "Chance": gChan, "Time_Taken": gTT };
+
+        for (let childElement of link.children) {
+          if (childElement.tagName === 'H3') {
+            if (gRes === 'Benign') {
+              childElement.style.backgroundColor = "green";
+              childElement.style.color = "black";
+            } else {
+              childElement.style.backgroundColor = "red";
+              childElement.style.color = "white";
+            }
+          }
         }
+
+      } else {
+        console.log("Fetching failed");
       }
-    })();
+      linkCnt++;
+      if (linkCnt == links.length){
+        console.log(`Calculating... (${linkCnt}/${links.length})`)
+        console.log('Finished Calculating !')
+      } else {
+        console.log(`Calculating... (${linkCnt}/${links.length})`)
+      }
+      
+    }
   }
-}, false);
+}
+
+
+function parseJsonVal(jsonVal, tok){
+  let finding = jsonVal.find(item => item.sesh_token === tok);
+  if (finding){
+    var result = finding["result"];
+    var chance = finding["chance"];
+    var time_taken = finding["time_taken"];
+    return [result, chance, time_taken]
+  } else {
+    console.log("No JSON Object Found!");
+  }
+}
+
+async function getPrediction() {
+  try{
+    const promise = await fetch('http://localhost:5000/get_url');
+    const promiseObj = await promise.json();
+    return promiseObj;
+  } catch (error){
+    console.log(`Ran into an Error: ${error}`)
+  }
+};
 
 // Forward HTTP Request with Method POST
 function predictSelection(selection, token){
